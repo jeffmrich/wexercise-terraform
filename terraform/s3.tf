48d94f28d69a923ef592c1d977e6d4949a92dcb1
Jeff Richards <richards.jeff@gmail.com>
2025-05-05 10:19:50 -0700

    Added aws_s3_bucket_public_access_block resource, to modify public acls

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -5,7 +5,15 @@ resource "aws_s3_bucket" "db_backup_bucket" {
   }
 }
 
-resource "aws_s3_bucket_policy" "too_open" {
+resource "aws_s3_bucket_public_access_block" "allow_public" {
+  bucket                  = aws_s3_bucket.db_backup_bucket.id
+  block_public_acls       = false
+  ignore_public_acls      = false
+  block_public_policy     = false   # <-- allow public policies
+  restrict_public_buckets = false   # <-- allow public access
+}
+
+resource "aws_s3_bucket_policy" "read_list_world" {
   bucket = aws_s3_bucket.db_backup_bucket.id
   policy = jsonencode({
     Version = "2012-10-17"
@@ -25,4 +33,5 @@ resource "aws_s3_bucket_policy" "too_open" {
       }
     ]
   })
+  depends_on = [aws_s3_bucket_public_access_block.allow_public]
 }
