5f59d195fa39034849bddffd877cfeec19924931
Jeff Richards <richards.jeff@gmail.com>
2025-05-05 09:19:40 -0700

    S3 bucket with world-read and list

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,6 +1,28 @@
 resource "aws_s3_bucket" "db_backup_bucket" {
-  bucket = "automode-cluster-five-bucket"
+  bucket = "automode-cluster-bucket"
   tags = {
-    Name = "automode-cluster-five-bucket"
+    Name = "automode-cluster-bucket"
   }
+}
+
+resource "aws_s3_bucket_policy" "too_open" {
+  bucket = aws_s3_bucket.my_bucket.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadList"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = [
+          "s3:GetObject",
+          "s3:ListBucket"
+        ]
+        Resource = [
+          "${aws_s3_bucket.my_bucket.arn}",
+          "${aws_s3_bucket.my_bucket.arn}/*"
+        ]
+      }
+    ]
+  })
 }
