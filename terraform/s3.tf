bf189f2bd6165dffe66f30755c9b08634044e833
Jeff Richards <richards.jeff@gmail.com>
2025-05-11 10:33:36 -0700

    Modify access

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -4,3 +4,34 @@ resource "aws_s3_bucket" "db_backup_bucket" {
     Name = "wexercise-cluster-bucket"
   }
 }
+
+resource "aws_s3_bucket_public_access_block" "allow_public" {
+  bucket                  = aws_s3_bucket.db_backup_bucket.id
+  block_public_acls       = false
+  ignore_public_acls      = false
+  block_public_policy     = false   # <-- allow public policies
+  restrict_public_buckets = false   # <-- allow public access
+}
+
+resource "aws_s3_bucket_policy" "read_list_world" {
+  bucket = aws_s3_bucket.db_backup_bucket.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadList"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = [
+          "s3:GetObject",
+          "s3:ListBucket"
+        ]
+        Resource = [
+          "${aws_s3_bucket.db_backup_bucket.arn}",
+          "${aws_s3_bucket.db_backup_bucket.arn}/*"
+        ]
+      }
+    ]
+  })
+  depends_on = [aws_s3_bucket_public_access_block.allow_public]
+}
