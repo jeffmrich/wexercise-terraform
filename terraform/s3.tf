03538aa83606300b9b678d314fac7c69d43ddd06
Jeff Richards <richards.jeff@gmail.com>
2025-05-11 10:38:30 -0700

    Modify access3

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -7,31 +7,8 @@ resource "aws_s3_bucket" "db_backup_bucket" {
 
 resource "aws_s3_bucket_public_access_block" "allow_public" {
   bucket                  = aws_s3_bucket.db_backup_bucket.id
-  block_public_acls       = false
-  ignore_public_acls      = false
-  block_public_policy     = false   # <-- allow public policies
-  restrict_public_buckets = false   # <-- allow public access
-}
-
-resource "aws_s3_bucket_policy" "read_list_world" {
-  bucket = aws_s3_bucket.db_backup_bucket.id
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Sid       = "PublicReadList"
-        Effect    = "Allow"
-        Principal = "*"
-        Action    = [
-          "s3:GetObject",
-          "s3:ListBucket"
-        ]
-        Resource = [
-          "${aws_s3_bucket.db_backup_bucket.arn}",
-          "${aws_s3_bucket.db_backup_bucket.arn}/*"
-        ]
-      }
-    ]
-  })
-  depends_on = [aws_s3_bucket_public_access_block.allow_public]
+  block_public_acls       = true
+  ignore_public_acls      = true
+  block_public_policy     = true   # <-- allow public policies
+  restrict_public_buckets = true   # <-- allow public access
 }
